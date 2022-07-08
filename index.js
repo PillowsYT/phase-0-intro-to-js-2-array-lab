8398262542cb2744de346351bd15112acdb46c06
Maple Thrower <dinzobros@gmail.com>
2022-07-08 04:50:05 -0400

    commit tree acc0ba3efa204f93527ad5d023eeaa43ca8757b4

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+  function destructivelyAppendCat () {
+    cats.push("Ralph")
+  }
+
+  function destructivelyPrependCat() {
+    cats.unshift("Bob")
+  }
+
+  function destructivelyRemoveLastCat() {
+    cats.pop ("Garfield")
+  }
+  
+  function destructivelyRemoveFirstCat() {
+    cats.shift("Milo")
+  }
+
+  function appendCat() {
+    appendCat = "Bruce"
+  }
